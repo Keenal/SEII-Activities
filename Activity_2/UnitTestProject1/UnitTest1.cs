6a0bc9ac259247350d95b8e010e1b3637ed65605
Keenal <kms118@students.uwf.edu>
2018-03-09 00:25:09 -0600

    add activity 2 files

---
+++ Activity_2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Activity2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApp1 
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestUSDtoPounds()
+        {
+            // Arrange
+            double startingAmount = 100.0;
+
+            var x = new Currency(CurrencyType.USD);
+            double expected = 65.0;
+
+            // Act 
+            double finalAmount = x.ConvertCurrency(startingAmount, CurrencyType.Pounds);
+
+            // Assert 
+
+            Assert.AreEqual(expected, finalAmount);
+        }
+        [TestMethod]
+        public void TestUSDtoEuros()
+        {
+            // Arrange
+            double startingAmount = 100.0;
+
+            var x = new Currency(CurrencyType.USD);
+            double expected = 88.0;
+
+            // Act 
+            double finalAmount = x.ConvertCurrency(startingAmount, CurrencyType.Euros);
+
+            // Assert 
+
+            Assert.AreEqual(expected, finalAmount);
+
+        }
+        [TestMethod]
+        public void TestPoundstoUSD()
+        {
+            // Arrange
+            double startingAmount = 100.0;
+
+            var x = new Currency(CurrencyType.Pounds);
+            double expected = 154.0;
+
+            // Act 
+            double finalAmount = x.ConvertCurrency(startingAmount, CurrencyType.USD);
+
+            // Assert 
+
+            Assert.AreEqual(expected, finalAmount);
+
+        }
+    }
+}
---
+++ Activity_2/UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Activity2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestCurrencyService()
+        {
+            double amount = 100.0;
+            var currency = new Currency(CurrencyType.USD);
+           // Currency startingType = CurrencyType.USD;
+          //  Currency endingType = CurrencyType.Euros;
+            var mockRepository = new Mock<ICurrencyRepository>();
+
+            mockRepository.Setup(r => r.getCurrency(CurrencyType.USD)).Returns(currency);
+            var sut = new CurrencyService(mockRepository.Object);
+
+
+            double actual = sut.convertCurrency(amount, CurrencyType.USD, CurrencyType.Euros);
+            double expected = 88.0;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
